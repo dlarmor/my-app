7524c60dd3f5f2ee53a17b5791316f71aa777bf7
start.vaadin.com <no-reply@vaadin.com>
2024-04-27 06:55:58 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/mainview/MainViewView.java
@@ -0,0 +1,42 @@
+package com.example.application.views.mainview;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.dependency.Uses;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+
+@PageTitle("MainView")
+@Menu(icon = "line-awesome/svg/pencil-ruler-solid.svg", order = 0)
+@Route(value = "my-view")
+@RouteAlias(value = "")
+@Uses(Icon.class)
+public class MainViewView extends Composite<VerticalLayout> {
+
+    public MainViewView() {
+        HorizontalLayout layoutRow2 = new HorizontalLayout();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        VerticalLayout layoutColumn3 = new VerticalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow2.addClassName(Gap.MEDIUM);
+        layoutRow2.setWidth("100%");
+        layoutRow2.setHeight("min-content");
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        layoutColumn3.setWidth("100%");
+        layoutColumn3.getStyle().set("flex-grow", "1");
+        getContent().add(layoutRow2);
+        getContent().add(layoutRow);
+        layoutRow.add(layoutColumn2);
+        layoutRow.add(layoutColumn3);
+    }
+}
